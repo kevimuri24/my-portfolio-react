eb43b9462b511c868f596b251cf09a3e861900d1
Kevin Muriuki Karimi <kevimuri24@gmail.com>
2026-01-13 18:27:06 -0500

    Fixed sizing issue on message box

--- my-portfolio/src/components/Contact.jsx
+++ my-portfolio/src/components/Contact.jsx
@@ -1,4 +1,4 @@
-import { useState, useEffect } from "react";
+import { useState } from "react";
 import { Col, Container, Row } from "react-bootstrap";
 import contactImg from "../assets/img/contact-img.svg"
 
@@ -55,29 +55,50 @@ export const Contact = () => {
                     <Col md={6}>
                         <h2>Get In Touch</h2>
                         <form onSubmit={handleSubmit}>
-                            <Row>
-                                <Col sm={6} className="px-1">
-                                    <input type="text" value={formDetails.firstname} placeholder="First Name" onChange={(e) => onFormUpdate('firstName', e.target.value)}/>
-                                </Col>
-                                <Col sm={6} className="px-1">
-                                    <input type="text" value={formDetails.lastname} placeholder="Last Name" onChange={(e) => onFormUpdate('lastName', e.target.value)}/>
-                                </Col>
-                                <Col sm={6} className="px-1">
-                                    <input type="email" value={formDetails.email} placeholder="Email" onChange={(e) => onFormUpdate('email', e.target.value)}/>
-                                </Col>
-                                <Col>
-                                <textarea rows="6" value={formDetails.message} placeholder="Message" onChange={(e) => onFormUpdate('message', e.target.value)}/>
-                                <button type="submit"><span>{buttonText}</span></button>
-                                </Col>
-                                {
-                                    status.message &&
-                                    <Col>
-                                        <p className={status.success === false ? "danger" : "success"}>{status.message}</p>
-                                    </Col>
-                                }
-                            </Row>
+                        <Row>
+                            <Col sm={6} className="px-1">
+                            <input
+                                type="text"
+                                value={formDetails.firstName}
+                                placeholder="First Name"
+                                onChange={(e) => onFormUpdate("firstName", e.target.value)}
+                            />
+                            </Col>
 
+                            <Col sm={6} className="px-1">
+                            <input
+                                type="text"
+                                value={formDetails.lastName}
+                                placeholder="Last Name"
+                                onChange={(e) => onFormUpdate("lastName", e.target.value)}
+                            />
+                            </Col>
+                        </Row>
+
+                        <Row>
+                            <Col className="px-1">
+                            <input
+                                type="email"
+                                value={formDetails.email}
+                                placeholder="Email"
+                                onChange={(e) => onFormUpdate("email", e.target.value)}
+                            />
+                            </Col>
+                        </Row>
+
+                        <Row>
+                            <Col className="px-1">
+                            <textarea
+                                rows={6}
+                                value={formDetails.message}
+                                placeholder="Message"
+                                onChange={(e) => onFormUpdate("message", e.target.value)}
+                            />
+                            <button type="submit"><span>{buttonText}</span></button>
+                            </Col>
+                        </Row>
                         </form>
+
                     </Col>
                 </Row>
             </Container>
